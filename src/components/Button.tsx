da015aca555203cdfc6c4cd9fd0815d2a841fb5f
Waleed Ahmed <waleedahmedwaheed@gmail.com>
2026-03-24 19:01:20 +0500

    updated minor version

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -19,7 +19,7 @@ const Button: React.FC<ButtonProps> = ({
     <button
       type={type}
       onClick={onClick}
-      className={`px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700 transition ${className}`}
+      className={`px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-700 transition ${className}`}
       disabled={disabled}
     >
       {children}
